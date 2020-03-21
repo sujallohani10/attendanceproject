8decdeeaf4c682704a1dd6a84a12bba94cb90b8d
sujallohani10 <sujal.lohani7@gmail.com>
2020-03-21 18:02:49 +1100

    migrating to mysql database

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import TimeSheet
+
 
 # Register your models here.
+class TimesheetAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'time_in', 'time_out', 'num_of_hours', 'status')
+
+
+admin.site.register(TimeSheet, TimesheetAdmin)
